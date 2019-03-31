99b998e4fbe65efc2ad82b9c77c5312f9f98b568
Oren (electricessence) <oren@electrified.net>
2019-03-30 21:43:57 -0700

    Updates.

--- SpanComparer.cs
+++ SpanComparer.cs
@@ -28,31 +28,31 @@ namespace Open.Memory
 		public static class Float
 		{
 			public static int Compare(in ReadOnlySpan<float> x, in ReadOnlySpan<float> y)
-			=> SpanComparer.Compare(x, y, (a, b) =>
-			{
-				if (float.IsNaN(a))
-					return float.IsNaN(b) ? 0 : -1;
-				else if (float.IsNaN(b))
-					return +1;
+				=> SpanComparer.Compare(x, y, (a, b) =>
+				{
+					if (float.IsNaN(a))
+						return float.IsNaN(b) ? 0 : -1;
+					else if (float.IsNaN(b))
+						return +1;
 
-				//if (a == b || Math.Abs(a - b) <= float.Epsilon && a.ToString() == b.ToString()) return 0; // We hate precision issues. :(  1==1 dammit!
-				return a.CompareTo(b);
-			});
+					//if (a == b || Math.Abs(a - b) <= float.Epsilon && a.ToString() == b.ToString()) return 0; // We hate precision issues. :(  1==1 dammit!
+					return a.CompareTo(b);
+				});
 		}
 
 		public static class Double
 		{
 			public static int Compare(in ReadOnlySpan<double> x, ReadOnlySpan<double> y)
-			=> SpanComparer.Compare(x, y, (a, b) =>
-			{
-				if (double.IsNaN(a))
-					return double.IsNaN(b) ? 0 : -1;
-				else if (double.IsNaN(b))
-					return +1;
+				=> SpanComparer.Compare(x, y, (a, b) =>
+				{
+					if (double.IsNaN(a))
+						return double.IsNaN(b) ? 0 : -1;
+					else if (double.IsNaN(b))
+						return +1;
 
-				//if (a == b || Math.Abs(a - b) < 0.00000001 && a.ToString() == b.ToString()) return 0; // We hate precision issues. :(  1==1 dammit!
-				return a.CompareTo(b);
-			});
+					//if (a == b || Math.Abs(a - b) < 0.00000001 && a.ToString() == b.ToString()) return 0; // We hate precision issues. :(  1==1 dammit!
+					return a.CompareTo(b);
+				});
 		}
 	}
 
---
+++ SpanExtensions.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Open.Memory
+{
+	public static class SpanExtensions
+	{
+
+		/// <summary>
+		/// Finds the first instance of a sequence and returns the set of values up to that sequence and emits the remaining as an out value.
+		/// </summary>
+		/// <param name="source">The source set to look through.</param>
+		/// <param name="character">The sequence to find.</param>
+		/// <param name="remaining">The remaining set after the sequence.  Will include entire source sequence if not found.</param>
+		/// <returns>The portion of the source up to and excluding the sequence searched for.</returns>
+		public static ReadOnlySpan<T> FirstSplit<T>(this in ReadOnlySpan<T> source,
+			in ReadOnlySpan<T> splitSequence,
+			out ReadOnlySpan<T> remaining)
+			where T : IEquatable<T>
+		{
+			if (splitSequence.Length == 0)
+				throw new ArgumentException("Cannot split using empty sequence.", nameof(splitSequence));
+
+			var i = source.Length == 0 ? -1 : source.IndexOf(splitSequence);
+			if (i == -1)
+			{
+				remaining = ReadOnlySpan<T>.Empty;
+				return source;
+			}
+
+			remaining = source.Slice(i + 1);
+			return source.Slice(0, i);
+		}
+
+		/// <summary>
+		/// Finds the first instance of a character sequence and returns the set of characters up to that sequence and emits the remaining as an out parameter.
+		/// </summary>
+		/// <param name="source">The source characters to look through.</param>
+		/// <param name="character">The sequence to find.</param>
+		/// <param name="remaining">The remaining characters after the sequence.  Will include entire source sequence if not found.</param>
+		/// <param name="comparisonType">The string comparison type to use.</param>
+		/// <returns>The portion of the source up to and excluding the sequence searched for.</returns>
+		public static ReadOnlySpan<char> FirstSplit(this in ReadOnlySpan<char> source,
+			in ReadOnlySpan<char> splitSequence,
+			out ReadOnlySpan<char> remaining,
+			StringComparison comparisonType)
+		{
+			if (splitSequence.Length == 0)
+				throw new ArgumentException("Cannot split using empty sequence.", nameof(splitSequence));
+
+			var i = source.Length == 0 ? -1 : source.IndexOf(splitSequence, comparisonType);
+			if (i == -1)
+			{
+				remaining = ReadOnlySpan<char>.Empty;
+				return source;
+			}
+
+			remaining = source.Slice(i + 1);
+			return source.Slice(0, i);
+		}
+
+		/// <summary>
+		/// Finds the first instance of a character and returns the set of characters up to that character and emits the remaining as an out parameter.
+		/// </summary>
+		/// <param name="source">The source characters to look through.</param>
+		/// <param name="character">The character to find.</param>
+		/// <param name="remaining">The remaining characters after the character.  Will include entire source sequence if not found.</param>
+		/// <returns>The portion of the source up to and excluding the character searched for.</returns>
+		public static ReadOnlySpan<T> FirstSplit<T>(this in ReadOnlySpan<T> source,
+			in T character,
+			out ReadOnlySpan<T> remaining)
+			where T : IEquatable<T>
+		{
+			var i = source.Length == 0 ? -1 : source.IndexOf(character);
+			if (i == -1)
+			{
+				remaining = ReadOnlySpan<T>.Empty;
+				return source;
+			}
+
+			remaining = source.Slice(i + 1);
+			return source.Slice(0, i);
+		}
+
+	}
+}
