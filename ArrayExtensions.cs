74152415041e329e66fb23e6ff0cc58e4f653157
Oren (electricessence) <oren@electrified.net>
2019-07-03 02:47:29 -0700

    Added ArrayPool extensions.

--- ArrayExtensions.cs
+++ ArrayExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Buffers;
 
 namespace Open.Memory
 {
@@ -55,6 +56,42 @@ namespace Open.Memory
 		public static bool IsGreaterThan<T>(this in ReadOnlyMemory<T> target, in ReadOnlyMemory<T> other)
 			where T : IComparable<T>
 			=> SpanComparer.Compare(target.Span, other.Span) > 0;
+
+		/// <summary>
+		/// Rents a buffer from the ArrayPool but returns a DisposeHandler with the buffer as it's value.
+		/// Facilitiates containing the temporary use of a buffer within a using block.
+		/// If the mimimumLength exceeds 1024*1024, an array will be created at that length for use.
+		/// </summary>
+		/// <typeparam name="T">The type of the array.</typeparam>
+		/// <param name="pool">The pool to get the array from.</param>
+		/// <param name="minimumLength">The minimum length of the array.</param>
+		/// <param name="clearArrayOnReturn">If true, will clear the array when it is returned to the pool.</param>
+		/// <returns>A DisposeHandler containing an array of type T[] that is at least minimumLength in length.</returns>
+		public static TemporaryArray<T> RentDisposable<T>(this ArrayPool<T> pool, int minimumLength, bool clearArrayOnReturn = false)
+		{
+			if (minimumLength < 0)
+				throw new ArgumentOutOfRangeException(nameof(minimumLength));
+
+			return new TemporaryArray<T>(pool, minimumLength, clearArrayOnReturn);
+		}
+
+		/// <summary>
+		/// Rents a buffer from the ArrayPool but returns a DisposeHandler with the buffer as it's value.
+		/// Facilitiates containing the temporary use of a buffer within a using block.
+		/// If the mimimumLength exceeds 1024*1024, an array will be created at that length for use.
+		/// </summary>
+		/// <typeparam name="T">The type of the array.</typeparam>
+		/// <param name="pool">The pool to get the array from.</param>
+		/// <param name="minimumLength">The minimum length of the array.  Must be no greater than Int32.MaxValue.</param>
+		/// <param name="clearArrayOnReturn">If true, will clear the array when it is returned to the pool.</param>
+		/// <returns>A DisposeHandler containing an array of type T[] that is at least minimumLength in length.</returns>
+		public static TemporaryArray<T> RentDisposable<T>(this ArrayPool<T> pool, long minimumLength, bool clearArrayOnReturn = false)
+		{
+			if (minimumLength < 0)
+				throw new ArgumentOutOfRangeException(nameof(minimumLength));
+
+			return TemporaryArray.Create(pool, minimumLength, clearArrayOnReturn);
+		}
 
 	}
 }
---
+++ TemporaryArray.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Buffers;
+
+namespace Open.Memory
+{
+
+	public struct TemporaryArray<T> : IDisposable
+	{
+		const int MaxLength = 1024 * 1024;
+		ArrayPool<T> Pool { get; set; }
+		public T[] Array { get; private set; }
+		public Span<T> Span
+			=> Array.Length == Length
+			? Array.AsSpan()
+			: Array.AsSpan().Slice(0, Length);
+
+		public int Length { get; }
+		public bool ClearOnReturn { get; }
+
+		public TemporaryArray(ArrayPool<T> pool, int length, bool clearOnReturn = false)
+		{
+			if (length < 0)
+				throw new ArgumentOutOfRangeException(nameof(length));
+
+			Length = length;
+			ClearOnReturn = clearOnReturn;
+
+			if (length == 0)
+			{
+				Pool = null;
+				Array = System.Array.Empty<T>();
+			}
+			if (pool==null || length > MaxLength)
+			{
+				Pool = null;
+				Array = new T[length];
+			}
+			else
+			{
+				Pool = pool;
+				Array = pool.Rent(length);
+			}
+		}
+
+		public void Dispose()
+		{
+			var a = Array;
+			Array = null;
+			Pool?.Return(a, true);
+			Pool = null;
+		}
+
+		public static implicit operator T[](TemporaryArray<T> source) => source.Array;
+		public static implicit operator Span<T>(TemporaryArray<T> source) => source.Span;
+	}
+
+	public static class TemporaryArray
+	{
+		public static TemporaryArray<T> Create<T>(ArrayPool<T> pool, int length, bool clearOnReturn = false)
+			=> new TemporaryArray<T>(pool, length, clearOnReturn);
+
+		public static TemporaryArray<T> Create<T>(ArrayPool<T> pool, long length, bool clearOnReturn = false)
+		{
+			if (length < 0 || length > int.MaxValue)
+				throw new ArgumentOutOfRangeException(nameof(length), length, "Must be at least zero and less than maximum 32 bit signed integer.");
+
+			return new TemporaryArray<T>(pool, (int)length, clearOnReturn);
+		}
+	}
+}
